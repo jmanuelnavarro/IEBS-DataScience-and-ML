96727f433d02343d97eae9bbb612a5041ce53205
Julio Navarro <jmanuel.navarro@gmail.com>
2020-07-15 09:07:03 +0200

    Ejercicios R

--- Clase2_Intro Lenguajes Programación/Intro R/test.R
+++ Clase2_Intro Lenguajes Programación/Intro R/test.R
@@ -32,3 +32,26 @@ df %>% summarise(avg_age = mean(Age, na.rm = TRUE)) %>% head()
 distinct(df, Age)
 
 df %>% group_by(Sex) %>% summarise(avg_age = mean(Age, na.rm = TRUE)) %>% head()
+
+# Generación de distribuciones
+
+gaussian_data <- rnorm(1000, mean = 0, sd = 1)
+mean(gaussian_data)
+sd(gaussian_data)
+
+hist(gaussian_data)
+boxplot(gaussian_data)
+plot(gaussian_data)
+
+uniform_data <- runif(1000, 0, 10)
+plot(uniform_data)
+hist(uniform_data)
+
+plot(gaussian_data, gaussian_data *4)
+
+noisy_data <- gaussian_data + uniform_data
+plot(noisy_data)
+hist(noisy_data)
+hist(gaussian_data)
+cor(gaussian_data, noisy_data)
+plot(gaussian_data, noisy_data)
